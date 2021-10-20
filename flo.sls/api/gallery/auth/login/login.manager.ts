6077091535fbcad0b9e3f56f9522283029272081
vozmak <strela996@bk.ru>
2021-10-20 11:43:30 +0300

    refactor: Transfer validation user form service to manager

--- flo.sls/api/gallery/auth/login/login.manager.ts
+++ flo.sls/api/gallery/auth/login/login.manager.ts
@@ -1,5 +1,8 @@
-import { HttpBadRequestError } from '@errors/http';
-import { Token, User } from './login.interface';
+import { HttpBadRequestError, HttpInternalServerError } from '@errors/http';
+import { UserScheme } from '@models/MongoDB';
+import { checkMongoConnect } from '@services/mongoDbConnect';
+import * as mongoose from 'mongoose';
+import { Token, User, VerifyUser } from './login.interface';
 import { LoginService } from './login.service';
 
 export class LoginManager {
@@ -9,11 +12,28 @@ export class LoginManager {
     this.service = new LoginService();
   }
 
-  logIn(user: User): Promise<Token> {
-    if (!user || !user.email || !user.password) {
-      throw new HttpBadRequestError('Email and password is required.');
+  async logIn(user: User): Promise<Token> {
+    let verifyUser: VerifyUser;
+
+    try {
+      await checkMongoConnect;
+      const Users = mongoose.models.Users || mongoose.model('Users', UserScheme, 'Users');
+
+      if (!user || !user.email || !user.password) {
+        throw new HttpBadRequestError('Email and password is required.');
+      }
+
+      verifyUser = await Users.findOne({ email: user.email });
+      if (!verifyUser) throw new HttpBadRequestError(`Пользователь ${user.email} не найден`);
+      // @ts-ignore
+      const validate = await verifyUser.isValidPassword(user.password);
+      if (!validate) throw new HttpBadRequestError('Неверный пароль');
+    } catch (e) {
+      if (e instanceof HttpBadRequestError) throw new HttpBadRequestError(e.message);
+
+      throw new HttpInternalServerError(e.message);
     }
 
-    return this.service.logIn(user);
+    return this.service.logIn(verifyUser);
   }
 }
--- flo.sls/api/gallery/auth/login/login.service.ts
+++ flo.sls/api/gallery/auth/login/login.service.ts
@@ -1,29 +1,17 @@
-import { HttpBadRequestError, HttpInternalServerError } from '@errors/http';
+import { HttpInternalServerError } from '@errors/http';
 import { getJWTToken } from '@helper/gallery/getJWTToken';
-import { UserScheme } from '@models/MongoDB';
-import { checkMongoConnect } from '@services/mongoDbConnect';
-import * as mongoose from 'mongoose';
-import { Token, User, VerifyUser } from './login.interface';
+import { Token, VerifyUser } from './login.interface';
 
 export class LoginService {
-  async logIn(user: User): Promise<Token> {
-    let verifyUser: VerifyUser;
+  async logIn(verifyUser: VerifyUser): Promise<Token> {
+    let token: Token;
 
     try {
-      await checkMongoConnect;
-      const Users = mongoose.models.Users || mongoose.model('Users', UserScheme, 'Users');
-
-      verifyUser = await Users.findOne({ email: user.email });
-      if (!verifyUser) throw new HttpBadRequestError(`Пользователь ${user.email} не найден`);
-      // @ts-ignore
-      const validate = await verifyUser.isValidPassword(user.password);
-      if (!validate) throw new HttpBadRequestError('Неверный пароль');
+      token = { token: getJWTToken(verifyUser) };
     } catch (e) {
-      if (e instanceof HttpBadRequestError) throw new HttpBadRequestError(e.message);
-
       throw new HttpInternalServerError(e.message);
     }
 
-    return { token: getJWTToken(verifyUser) };
+    return token;
   }
 }
